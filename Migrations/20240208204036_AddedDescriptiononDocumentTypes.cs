9b671105c9c144144e56ce75c89c86c653b229d6
thanosch94 <chatzopoulosthanos@gmail.com>
2024-02-08 22:43:35 +0200

    Added Migration for Document Types

---
+++ Migrations/20240208204036_AddedDescriptiononDocumentTypes.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataNex.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDescriptiononDocumentTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "datanex_documenttypes",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "datanex_documenttypes",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "datanex_documenttypes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "datanex_documenttypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
