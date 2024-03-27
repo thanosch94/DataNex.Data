9798738a5d03a8816c9d573c5c3b79750451ad68
thanosch94 <chatzopoulosthanos@gmail.com>
2024-03-27 22:52:42 +0200

    Changed Product Image type on Database

---
+++ Migrations/20240327205121_ChangedImageToImagePath.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataNex.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedImageToImagePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "datanex_products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "datanex_products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "datanex_products");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "datanex_products",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
