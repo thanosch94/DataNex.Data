a7fafe3f94f37876b12212212130b96699253e2d
thanosch94 <chatzopoulosthanos@gmail.com>
2024-05-07 20:45:36 +0300

    Ver 1.00

---
+++ AppBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Data
+{
+    public static class AppBase
+    {
+        public static decimal Version = 1.00M;
+    }
+}
