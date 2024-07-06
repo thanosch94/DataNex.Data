0d73274b096239b8d6cf7d0b868f45f969c56194
thanosch94 <chatzopoulosthanos@gmail.com>
2024-07-06 20:55:53 +0300

    Seeded Statuses

--- DataSeedIds.cs
+++ DataSeedIds.cs
@@ -8,6 +8,7 @@ namespace DataNex.Data
 {
     public class DataSeedIds
     {
+        //Document Types
         public static Guid Offer = Guid.Parse("ea9422a4-c930-44b4-bb1e-a87a019cf716");
         public static Guid SalesOrder = Guid.Parse("580f43bd-0b77-4a93-b95e-b04f851463ea");
         public static Guid ProformaInvoice = Guid.Parse("3a7da7f5-93e7-40a6-8e62-6a2836ecec96");
@@ -18,6 +19,14 @@ namespace DataNex.Data
         public static Guid PurchaseOrder = Guid.Parse("270dce95-9562-4c63-86ee-0aeca760ab9b");
         public static Guid PurchaseInvoice = Guid.Parse("c7825a32-6007-48b4-936d-f558c9b788c3");
         public static Guid CreditNote = Guid.Parse("4437b7c7-c4a1-4de9-ace3-4e807b9a5f13");
+
+
+        //Statuses
+        public static Guid Pending = Guid.Parse("6462d79c-6289-4779-9aaa-1d182c512dab");
+        public static Guid OnHold = Guid.Parse("4364282a-7df2-4c7a-9c2a-49cef6f14143");
+        public static Guid Processing = Guid.Parse("a28ec2c6-6195-4dfd-95ce-c428f675c810");
+        public static Guid Completed = Guid.Parse("3fe07a77-418b-4a79-a625-5093b6d88da6");
+        public static Guid Returned = Guid.Parse("e72dbda9-e8dd-4b46-8dc5-c182d78a91ac");
 
     }
 }
